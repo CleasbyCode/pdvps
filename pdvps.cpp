fc725dc96b6f9190586add3ed64bc6a4c113dc02
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2022-11-13 16:48:36 +0000

    Update pdvps.cpp

--- pdvps.cpp
+++ pdvps.cpp
@@ -10,8 +10,9 @@
 int main(int argc, char** argv) {
 	if (argc == 2 && std::string(argv[1]) == "--info") {
 		std::cout << "\nPNG Data Vehicle for Twitter, PowerShell Edition (PDVPS) v1.0. Created by Nicholas Cleasby (@CleasbyCode) 1/11/2022.\n\n"
-			 << "PDVPS enables you to embed any file (max file size 5,234,608 bytes) within a tweetable and 'executable' PNG image.\n\n"
-			 << "For further information, please visit https://github.com/CleasbyCode/pdvps\n\n";}
+			<< "PDVPS enables you to embed any file (max file size 5,234,608 bytes) within a tweetable and 'executable' PNG image.\n\n"
+			<< "For further information, please visit https://github.com/CleasbyCode/pdvps\n\n";
+	}
 	else if (argc < 2 || argc > 2) {
 		std::cout << "\nUsage:  pdvps  <your_file>\n\tpdvps  --info\n\n";
 	}
@@ -20,13 +21,13 @@ int main(int argc, char** argv) {
 
 		// Attempt to open user data file. Display error and quit program if file fails to open.
 		std::ifstream readData(DATA_FILE, std::ios::binary);
-		if (!readData) { 
+		if (!readData) {
 			std::cerr << "\nData File Read Error : Unable to open/read file : " + DATA_FILE + "\n\n";
 			return -1;
 		}
-		
-		// Default image with embedded scripts has an 8K overhead - 5MB (Twitter PNG size limit) = 5,234,608 bytes available for arbiraty data.
-		const int MAX_FILE_SIZE_BYTES = 5234608;
+
+		// Default image with embedded scripts has an 9K overhead - 5MB (Twitter PNG size limit) = 5,233,664 bytes available for arbiraty data.
+		const int MAX_FILE_SIZE_BYTES = 5233664;
 
 		// Get and check user filesize. Display error and quit program if file size exceeds max size.
 		readData.seekg(0, readData.end);
@@ -38,29 +39,29 @@ int main(int argc, char** argv) {
 		}
 		// Convert user data file size value to a string. 
 		// This string will be inserted into the '$fl' (File Length) variable for the embedded PowerShell (PS) script.
-		std::string dataSizeLengthString = std::to_string(DATA_SIZE), 
-					argsLinux,	// Store user command-line arguments here for Linux.
-							// Contents will be inserted into '$la' (Linux Arguments) variable for the embedded PS script.
+		std::string dataSizeLengthString = std::to_string(DATA_SIZE),
+			argsLinux,	// Store user command-line arguments here for Linux.
+					// Contents will be inserted into '$la' (Linux Arguments) variable for the embedded PS script.
 
-					argsWindows,	// Store user command-line arguments here for Windows.
-							// Contents will be inserted into '$wa' (Windows Arguments) variable for the embedded PS script.
+			argsWindows,	// Store user command-line arguments here for Windows.
+					// Contents will be inserted into '$wa' (Windows Arguments) variable for the embedded PS script.
 
-					ext;		// File extension (of user data file) string variable.
-							// Contents will be inserted into '$ext' (extension) variable for the embedded PS script.
-									
+			ext;		// File extension (of user data file) string variable.
+					// Contents will be inserted into '$ext' (extension) variable for the embedded PS script.
+
 		// int array contains index locations for inserting data items into the embedded PowerShell (PS) script, within the PNG image.
-		// [0] = insert index location for the PS 'repair code' strings (see below).
-		// [1] = insert index location for the PS '$la' Linux Arguments string variable.
-		// [2] = insert index location for the PS '$wa' Windows Arguments string variable.
-		// [3] = insert index location for the PS '$ext' File Extension of user data file, string variable.
-		// [4] = insert index location for the PS '$fl', File Length value of user data file, variable.
+		// [0] value is the insert index location for the PS 'repair code' strings (see below).
+		// [1] value is the insert index location for the PS '$la' Linux Arguments string variable.
+		// [2] value is the insert index location for the PS '$wa' Windows Arguments string variable.
+		// [3] value is the insert index location for the PS '$ext' File Extension of user data file, string variable.
+		// [4] value is the insert index location for the PS '$fl', File Length value of user data file, variable.
 		// For more information about the PowerShell script, see the "scripts_info.txt" file that's part of this repo on GitHub. 
-		int pwshInsertIndex[5] = { DATA_SIZE + 2608, DATA_SIZE + 2173, DATA_SIZE + 2166, DATA_SIZE + 2095, DATA_SIZE + 2088 }, 
+		int pwshInsertIndex[5] = { DATA_SIZE + 2724, DATA_SIZE + 2289, DATA_SIZE + 2282, DATA_SIZE + 2179, DATA_SIZE + 2172 },
 
-			nameLength = DATA_FILE.length(), 
-			dot = DATA_FILE.find_last_of('.'), 
-			histChunkLengthInsertIndex = 2035,	// Index location for hIST chunk length field. Use this location value when we insert chunk length.
-			histChunkLength = 1204,			// Initial hIST chunk size. This will grow as additional data items are inserted into the PS script.
+			nameLength = DATA_FILE.length(),
+			dot = DATA_FILE.find_last_of('.'),
+			histChunkLengthInsertIndex = 2119,	// Index location for hIST chunk length field. Use this location value when we insert chunk length.
+			histChunkLength = 1236,			// Initial hIST chunk size. This will grow as additional data items are inserted into the PS script.
 			bits = 24;
 
 		// If user file does not have an extension, assume its an executable file, ext will be '.exe'.
@@ -77,19 +78,19 @@ int main(int argc, char** argv) {
 		// If user data file is Python, PowerShell or an Executable file (.exe), you can add command-line arguments for these files, if required.
 		if (ext == ".py" || ext == ".ps1" || ext == ".exe") {
 			const int MAX_ARGS_LENGTH = 100;  // Set a limit of the character length of command-line arguments.
-			std::cout << "\nFor this file type you can provide command-line arguments here, if required.\n\nLinux: "; 
+			std::cout << "\nFor this file type you can provide command-line arguments here, if required.\n\nLinux: ";
 			std::getline(std::cin, argsLinux);
-			std::cout << "\nWindows: "; 
+			std::cout << "\nWindows: ";
 			std::getline(std::cin, argsWindows);
 			if (argsLinux.length() > MAX_ARGS_LENGTH || argsWindows.length() > MAX_ARGS_LENGTH) {
 				std::cerr << "\nError: Arguments exceed maximum length.\n\n";
 				return -1;
 			}
 		}
-		const int DATA_FILE_INSERT_INDEX = 2043; // Index location of where user data file is inserted into the default PNG image file.
+		const int DATA_FILE_INSERT_INDEX = 2127; // Index location of where user data file is inserted into the default PNG image file.
 
 		// Read in user data file into above index location of default PNG image and 'encrypt' file using simple XOR method. 
-		char byte; 
+		char byte;
 		int j = 704;	// Set variable j to the 704th index location of the default PNG file.
 
 		std::ifstream readFile(DATA_FILE, std::ios::binary);
@@ -105,26 +106,26 @@ int main(int argc, char** argv) {
 
 		// The user data file CANNOT contain any occurrence of the PowerShell end-comment-block string '#>'.
 		// This will cause the embedded PowerShell script to fail if it encounters that string anywhere in the user file. 
-		
+
 		// The C++ program will check the file and if found, modify a byte for every occurrence of this string.
 		// It will store index locations of each modified byte in a larger string containing PowerShell code, which will put back the 
 		// original '#' character that was changed, before writing the extracted user file to disk.
 
-		const std::string BLOCK_ID = "#>"; 
+		const std::string BLOCK_ID = "#>";
 		int pwshRepairCodeLength = 0;
-		
+
 		// Search user data file for end-comment block string.
 		if (search(ImageVec.begin(), ImageVec.end(), BLOCK_ID.begin(), BLOCK_ID.end()) - ImageVec.begin() <= DATA_SIZE + DATA_FILE_INSERT_INDEX) {
 			std::string index_locations = "";
 			for (int BLOCK_INDEX = 0; DATA_SIZE + DATA_FILE_INSERT_INDEX > BLOCK_INDEX;) {
-				if (BLOCK_INDEX > 0) { 
+				if (BLOCK_INDEX > 0) {
 					// Build up a string containing index locations for every end-comment-block found and modified within user data file
 					index_locations += std::to_string(BLOCK_INDEX) + ',';
 					ImageVec[BLOCK_INDEX] = 'X';  // change the '#' character of each end-comment-block string found.
-				} 
+				}
 				BLOCK_INDEX = search(ImageVec.begin() + BLOCK_INDEX + 1, ImageVec.end(), BLOCK_ID.begin(), BLOCK_ID.end()) - ImageVec.begin();
 			}
-			
+
 			// String contains PS 'repair code' to be inserted into embedded PS script within the PNG image.
 			// PS array '$idx=@("<index_locations>")' will contain index location values for each byte we need to change back to original '#' character.
 			// Index location values are created by the this C++ program, see 'for loop' above.
@@ -149,10 +150,10 @@ int main(int argc, char** argv) {
 
 		// Insert file extension string into PowerShell script variable '$ext' at correct index location.
 		ImageVec.insert(ImageVec.begin() + pwshInsertIndex[3], ext.begin(), ext.end());
-		
+
 		// Insert user file data length string into PowerShell script variable '$fl' at correct index location. 
 		ImageVec.insert(ImageVec.begin() + pwshInsertIndex[4], dataSizeLengthString.begin(), dataSizeLengthString.end());
-		
+
 		// Add to the current hIST chunk value the length values of all the data items we have inserted into the PowerShell script.
 		// This should now be the correct and final hIST chunk length.
 		histChunkLength += DATA_SIZE + pwshRepairCodeLength + ext.length() + dataSizeLengthString.length() + argsLinux.length() + argsWindows.length();
