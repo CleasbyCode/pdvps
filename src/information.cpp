10f5fc407641b0db1db5fc3fd89ab5f1feaf8077
Nick Cleasby <113206035+CleasbyCode@users.noreply.github.com>
2024-10-07 17:47:49 +0100

    Update information.cpp

--- src/information.cpp
+++ src/information.cpp
@@ -1,11 +1,11 @@
 void displayInfo() {
 	std::cout << R"(
-PNG Data Vehicle, PowerShell Edition (PDVPS v1.0.2). 
+PNG Data Vehicle, PowerShell Edition (PDVPS v1.2). 
 Created by Nicholas Cleasby (@CleasbyCode) 1/11/2022.
 
 Embed any file type within the default supplied PNG image to create a tweetable and 'executable' image.
 
-Post your file-embedded PNG image on X/Twitter (5MB max.).
+Post your "file-embedded" PNG image on X/Twitter (5MB max.).
 
 For further information, https://github.com/CleasbyCode/pdvps
 
